e709be00bd5906e54e1cceb84b9fa9c5a5c79d5f
Kevin Moreno <kevinparra192@gmail.com>
2025-06-01 18:00:53 -0700

    ADDED | Cors

--- app.js
+++ app.js
@@ -9,9 +9,21 @@ const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
+
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000", "http://127.0.0.1:3000"];
+
 app.use(cors({
-    origin: "http://localhost:5173"
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
+
 app.use(morgan('dev')); 
 
 
